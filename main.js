29f7805832bb80be646bed80ebbee157ef75160b
rjw1428 <rjw1428@gmail.com>
2020-03-09 20:07:12 -0400

    App window starts up

---
+++ main.js
@@ -0,0 +1,33 @@
+import { BrowserWindow } from "electron"
+
+//BACKEND LOGIC
+
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    backgroudColor: '#000',
+    icon: `file://${{__dirname}}/dist/assets/log.png`
+  })
+
+  win.loadURL(`file://${__dirname}/dist/index.html`)
+  // win.webContents.openDevTools()
+
+  win.on('closed', () => win = null)
+  app.on('ready', createWindow)
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+      app.quit()
+  })
+  app.on('active', () => {
+    if (win === null)
+      createWindow()
+  })
+}
